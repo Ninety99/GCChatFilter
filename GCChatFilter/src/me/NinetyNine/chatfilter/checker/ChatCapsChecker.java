94286899ef73e5916ded15c4f405cddc74b32e36
BuildTools <unconfigured@null.spigotmc.org>
2018-07-28 15:53:29 +0800

    Fixed? plz

--- GCChatFilter/src/me/NinetyNine/chatfilter/checker/ChatCapsChecker.java
+++ GCChatFilter/src/me/NinetyNine/chatfilter/checker/ChatCapsChecker.java
@@ -10,28 +10,37 @@ public class ChatCapsChecker implements Listener {
 
 	@EventHandler
 	public void onAsyncPlayerChat(AsyncPlayerChatEvent e) {
-		String msg = e.getMessage().trim();
-
-		int caps = 0;
-		String after = "";
-
-		String[] args = msg.split(" ");
-
-		for (String word : args)
-			if (ChatFilterConfig.getConfig().getStringList("whitelist").contains(word))
-				return;
-		
-		for (int i = 0; i < msg.length(); i++) {
-			if (Character.isUpperCase(msg.charAt(i)))
-				caps++;
-		}
-
-		if (caps > ChatFilterConfig.getInt("maxcaps"))
-			after = ("" + msg.charAt(0)).toUpperCase() + msg.substring(1).toLowerCase();
-
-		if (after.length() > 0)
-			e.setMessage(after);
-		else
+		if (e.getPlayer().hasPermission("gcf.bypass"))
 			return;
+		else {
+			String msg = e.getMessage().trim();
+
+			int caps = 0;
+			String after = "";
+
+			String[] args = msg.split(" ");
+
+			for (int i = 0; i < msg.length(); i++) {
+				if (Character.isUpperCase(msg.charAt(i)))
+					caps++;
+			}
+
+			for (int i = 0; i < args.length; i++) {
+				if (ChatFilterConfig.getConfig().getStringList("whitelist").contains(args[i].toUpperCase()))
+					args[i].toUpperCase();
+			}
+
+			if (caps > ChatFilterConfig.getInt("maxcaps")) {
+				if (Character.isUpperCase(msg.charAt(0))) {
+					after = ("" + msg.charAt(0)).toUpperCase() + msg.substring(1).toLowerCase();
+				} else
+					after = msg.substring(1).toLowerCase();
+			}
+			
+			if (after.length() > 0)
+				e.setMessage(after);
+			else
+				return;
+		}
 	}
 }
