f6a92711f93f01f27f60ba6cc335f9bfaf48720b
BuildTools <unconfigured@null.spigotmc.org>
2018-08-04 14:13:06 +0800

    pls fix

--- GCChatFilter/src/me/NinetyNine/chatfilter/ChatCapsChecker.java
+++ GCChatFilter/src/me/NinetyNine/chatfilter/ChatCapsChecker.java
@@ -1,5 +1,7 @@
 package me.NinetyNine.chatfilter;
 
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
@@ -10,9 +12,8 @@ public class ChatCapsChecker implements Listener {
 	public void onAsyncPlayerChat(AsyncPlayerChatEvent e) {
 		if (e.getPlayer().hasPermission("gcf.bypass"))
 			return;
-		else {
+		else
 			third(e);
-		}
 	}
 
 	public void third(AsyncPlayerChatEvent e) {
@@ -25,34 +26,40 @@ public class ChatCapsChecker implements Listener {
 		String after = "";
 
 		for (String word : args) {
-			System.out.println("got the oldWord");
-
+			System.out.println(word + " is the word");
+			
+			for (Player on : Bukkit.getServer().getOnlinePlayers()) {
+				if (on.getName().equals(word))
+					return;
+			}
+			
 			for (int i = 0; i < word.length(); i++) {
 				if (Character.isUpperCase(word.charAt(i)))
 					caps++;
 				System.out.println("caps++");
-
-				if (ChatFilterConfig.getConfig().getStringList("whitelist").contains(word.toUpperCase()))
-					word.toUpperCase();
 			}
 
 
+			if (ChatFilterConfig.getConfig().getStringList("whitelist").contains(word.toUpperCase()))
+				word.toUpperCase();
+			
 			if (caps > ChatFilterConfig.getInt("maxcaps")) {
 				String newWord = word.toLowerCase();
-				if (Character.isUpperCase(message.charAt(0)))
-					after = ("" + message.charAt(0)).toUpperCase()
-							+ message.replace(word, newWord);
+				// (message.charAt(word.indexOf(word.length() - 1))
+				if (Character.isUpperCase(word.charAt(0)))
+					after = ("" + message.charAt(0)).toUpperCase() + message.replace(word, newWord);
 				else
 					after = message.replace(word, newWord);
 
-				System.out.println("replace");
+				System.out.println("replace " + word + " to " + newWord);
 
-				e.setMessage(after);
-				System.out.println("set message");
 				caps = 0;
 			} else
 				System.out.println("not caps");
+			word = after;
 		}
+		
+		e.setMessage(after);
 	}
 
 	public void test(AsyncPlayerChatEvent e) {
@@ -74,10 +81,8 @@ public class ChatCapsChecker implements Listener {
 		}
 
 		if (caps > ChatFilterConfig.getInt("maxcaps")) {
-			if (Character.isUpperCase(msg.charAt(0))) {
+			if (Character.isUpperCase(msg.charAt(0)))
 				after = ("" + msg.charAt(0)).toUpperCase() + msg.substring(1).toLowerCase();
-			} else
-				after = msg.substring(1).toLowerCase();
 
 			if (after.length() > 0)
 				e.setMessage(after);
